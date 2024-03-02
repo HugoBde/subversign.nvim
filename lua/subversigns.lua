3c4a89e0aa502b8478141912230b354bebb1eeb6
Hugo Bouderlique <bouderliqueh@gmail.com>
2024-03-02 23:31:03 +1100

    experimentation with vim api

--- lua/subversigns.lua
+++ lua/subversigns.lua
@@ -1,18 +1,56 @@
-local subversigns = {}
+M = {
+    highlight_name = "SubversignsSimpleHl",
+    lines = {}
+}
 
-function subversigns.setup()
-    vim.api.nvim_create_augroup("subversigns")
-    -- setup the onAttach function or something
-    vim.api.nvim_create_autocmd({ 'BufRead' }, {
-        group = 'subversigns',
-        callback = function(_)
-            subversigns.on_attach()
-        end
-    })
+---@type string
+
+--- Setup Subversigns
+function M.setup()
+    vim.print("M setup!")
+    M.ns = vim.api.nvim_create_namespace("Subversigns")
+    vim.api.nvim_set_hl(M.ns,
+        M.highlight_name,
+        {
+            fg = "#FF00FF"
+        })
+
+    vim.fn.sign_define("SubversignsSimpleSign",
+        {
+            text = "│",
+            texthl = M.highlight_name
+        })
+
+    vim.api.nvim_create_user_command("SubversignsToggle", M.toggle_sign, { desc = "toggle subversigns sign" })
+
+    vim.keymap.set("n", "<leader>ls", "<cmd>SubversignsToggle<CR>", { desc = "toggle sign" })
 end
 
-function on_attach()
-    vim.notify("Hehehe boi!", vim.log.levels.INFO)
+--- Set a sign
+function M.toggle_sign()
+    local win_nr = vim.api.nvim_get_current_win()
+    local buf_nr = vim.api.nvim_win_get_buf(win_nr)
+
+    local lnum = unpack(vim.api.nvim_win_get_cursor(win_nr), 1, 1)
+
+
+    if M.lines[tostring(lnum)] then
+        vim.print("removing sign on line " .. tostring(lnum));
+        vim.fn.sign_unplace("Subversigns", { lnum = lnum, id = lnum, buffer = buf_nr })
+        M.lines[tostring(lnum)] = false
+    else
+        vim.print("Setting sign on line " .. tostring(lnum));
+        vim.fn.sign_place(lnum,
+            "Subversigns",
+            "SubversignsSimpleSign",
+            buf_nr,
+            {
+                lnum = lnum,
+                priority = 10
+
+            })
+        M.lines[tostring(lnum)] = true
+    end
 end
 
-return subversigns
+return M
