8a3fe95dbc5266f8d62920b9c4c7baa928a46ac6
Hugo Bouderlique <bouderliqueh@gmail.com>
2024-03-03 23:44:01 +1100

    First draft
    - Define our 3 basic signs: SubversignsAdd, SubversignsChange, SubversignsDelete
    - Create a function that:
      - clears current signs in buffer
      - runs a diff on current buffer
      - parses output of diff command
      - add signs based on the parsed hunk headers
    - Create a command "SubversignsInit" that invokes the function above
    - Create an autocmd that executes "SubversignsInit" on "BufRead"

--- lua/subversigns.lua
+++ lua/subversigns.lua
@@ -1,55 +1,109 @@
-M = {
-    highlight_name = "SubversignsSimpleHl",
-    lines = {}
-}
+M = {}
 
----@type string
+local diff_line_pattern = vim.regex("^@@ .* @@$")
+
+--- Executes a diff on the current buffer
+---@return string the output of the diff command
+local function run_diff()
+    local buf_nr = vim.api.nvim_get_current_buf()
+    local file_name = vim.api.nvim_buf_get_name(buf_nr)
+    return vim.fn.system({ "svn", "diff", "--diff-cmd=/usr/bin/diff", "-x", "--normal", file_name })
+end
+
+
+--- Process a add hunk
+--- @param buf_nr integer
+--- @param start_line integer
+--- @param end_line integer
+local function add_hunk_process(buf_nr, start_line, end_line)
+    if (not end_line) then
+        vim.fn.sign_place(0, nil, "SubversignsAdd", buf_nr, { lnum = start_line, priority = 100 })
+    else
+        for i = start_line, end_line, 1 do
+            vim.fn.sign_place(0, nil, "SubversignsAdd", buf_nr, { lnum = i, priority = 100 })
+        end
+    end
+end
+
+--- Process a delete hunk
+--- @param buf_nr integer
+--- @param line integer
+local function delete_hunk_process(buf_nr, line)
+    vim.fn.sign_place(0, nil, "SubversignsDelete", buf_nr, { lnum = line, priority = 100 })
+end
+
+--- Process a change hunk
+--- @param buf_nr integer
+--- @param start_line integer
+--- @param end_line integer
+local function change_hunk_process(buf_nr, start_line, end_line)
+    if (not end_line) then
+        vim.fn.sign_place(0, nil, "SubversignsChange", buf_nr, { lnum = start_line, priority = 100 })
+    else
+        for i = start_line, end_line, 1 do
+            vim.fn.sign_place(0, nil, "SubversignsChange", buf_nr, { lnum = i, priority = 100 })
+        end
+    end
+end
 
 --- Setup Subversigns
 function M.setup()
-    vim.print("M setup!")
-    M.ns = vim.api.nvim_create_namespace("Subversigns")
-    vim.api.nvim_set_hl(M.ns,
-        M.highlight_name,
+    -- Define signs
+    vim.fn.sign_define("SubversignsAdd",
         {
-            fg = "#FF00FF"
+            text = "┃",
+            texthl = "GitSignsAdd"
         })
 
-    vim.fn.sign_define("SubversignsSimpleSign",
+    vim.fn.sign_define("SubversignsDelete",
         {
-            text = "│",
-            texthl = M.highlight_name
+            text = "▁",
+            texthl = "GitSignsDelete"
         })
 
-    vim.api.nvim_create_user_command("SubversignsToggle", M.toggle_sign, { desc = "toggle subversigns sign" })
+    vim.fn.sign_define("SubversignsChange",
+        {
+            text = "┃",
+            texthl = "GitSignsChange"
+        })
 
-    vim.keymap.set("n", "<leader>ls", "<cmd>SubversignsToggle<CR>", { desc = "toggle sign" })
+    -- Create user command to init Subversigns in current buffer
+    vim.api.nvim_create_user_command("SubversignsInit", M.subversign_my_buffer, { desc = "Init Subversigns in buffer" })
+
+    -- Setup autocmd to execute SubversignsInit when opening a new file
+    vim.api.nvim_create_autocmd("BufRead", { command = "SubversignsInit" })
 end
 
---- Set a sign
-function M.toggle_sign()
-    local win_nr = vim.api.nvim_get_current_win()
-    local buf_nr = vim.api.nvim_win_get_buf(win_nr)
+--- Set signs for a buffer
+function M.subversign_my_buffer()
+    local buf_nr = vim.api.nvim_get_current_buf()
 
-    local lnum = unpack(vim.api.nvim_win_get_cursor(win_nr), 1, 1)
+    -- clear existing signs
+    vim.fn.sign_unplace("Subversigns", { buffer = buf_nr })
 
+    -- run diff
+    local diff = run_diff()
 
-    if M.lines[tostring(lnum)] then
-        vim.print("removing sign on line " .. tostring(lnum));
-        vim.fn.sign_unplace("Subversigns", { lnum = lnum, id = lnum, buffer = buf_nr })
-        M.lines[tostring(lnum)] = false
-    else
-        vim.print("Setting sign on line " .. tostring(lnum));
-        vim.fn.sign_place(lnum,
-            "Subversigns",
-            "SubversignsSimpleSign",
-            buf_nr,
-            {
-                lnum = lnum,
-                priority = 10
+    -- for each all hunks, apply signs
+    for line in vim.gsplit(diff, "\n") do
+        local hunk_type, start_line, end_line = string.match(line, "%d,?%d?(.)(%d),?(%d?)")
 
-            })
-        M.lines[tostring(lnum)] = true
+        if (not hunk_type) then
+            goto continue
+        end
+
+        start_line = tonumber(start_line, 10)
+        end_line = tonumber(end_line, 10)
+
+        if (hunk_type == "a") then
+            add_hunk_process(buf_nr, start_line, end_line)
+        elseif (hunk_type == "c") then
+            change_hunk_process(buf_nr, start_line, end_line)
+        else
+            delete_hunk_process(buf_nr, start_line)
+        end
+
+        ::continue::
     end
 end
 
